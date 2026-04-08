2abee6070c67fc772b68a6255772d922bc0cdb95
Rawasi44 <rawasialmutairi01@gmail.com>
2026-04-08 10:57:37 +0300

    Add files via upload

--- routes/events.py
+++ routes/events.py
@@ -1,1 +1,16 @@
+from flask import Blueprint, render_template
+from models.event import Event
 
+events_bp = Blueprint("events", __name__)
+
+
+@events_bp.route("/events")
+def events_page():
+    events = Event.query.all()
+    return render_template("events.html", events=events)
+
+
+@events_bp.route("/events/<int:event_id>")
+def event_details(event_id):
+    event = Event.query.get_or_404(event_id)
+    return render_template("event_details.html", event=event)
