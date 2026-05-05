4148c2528d67d47e3745b1edb3c0290c92a5071d
Razan <razanppphdt90@gmail.com>
2026-05-05 16:39:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from extensions import db, login_manager
+from extensions import db, login_manager, mail
 
 def create_app():
     app = Flask(__name__)
@@ -8,6 +8,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
     login_manager.login_view = "auth.login"
 
     from models.user import User
